4625174f237f0ec5bfd992900bf5b11fa3a496df
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-21 20:19:09 +0300

    Create and Export Search Component
     - Search is a form element and a search button
    
     - this will be imported into Body Component

---
+++ src/Search.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="container-fluid">
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search Transactions"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
