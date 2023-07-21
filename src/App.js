1dea7e77186211bf9b938e7b4fc8473c8d041076
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-21 21:56:04 +0300

    render Footer Component in App instead of Body
     - import and render Footer on App
    
     - delete Footer on Body

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Body from "./Body";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Body />
+      <Footer />
     </>
   );
 }
--- src/Body.js
+++ src/Body.js
@@ -1,14 +1,14 @@
 import Search from "./Search";
 import Transactions from "./Transactions";
-import Footer from "./Footer";
 
 function Body() {
   return (
-    <body className="p-3 m-0 border-0 bd-example m-0 border-0">
-      <Search />
-      <Transactions />
-      <Footer />
-    </body>
+    <>
+      <body className="p-3 m-0 border-0 bd-example m-0 border-0">
+        <Search />
+        <Transactions />
+      </body>
+    </>
   );
 }
 
