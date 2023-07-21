f82cf7423b29254ea06e5d89b1a1543046eeae71
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-21 22:01:24 +0300

    fix: refactor Body component to fix errors
      - validateDOMNesting(...): <body> cannot appear as a child of <div>
      error appears since a <body> tag is used to render Body on a <div> in App
    
      - change <body> to <div> to fix error
    
      - add body class to the new div to style it appropriately
    
      - change background color to the nav to match the entire body

--- src/Body.js
+++ src/Body.js
@@ -4,10 +4,10 @@ import Transactions from "./Transactions";
 function Body() {
   return (
     <>
-      <body className="p-3 m-0 border-0 bd-example m-0 border-0">
+      <div className="p-3 m-0 border-0 bd-example m-0 border-0 body">
         <Search />
         <Transactions />
-      </body>
+      </div>
     </>
   );
 }
