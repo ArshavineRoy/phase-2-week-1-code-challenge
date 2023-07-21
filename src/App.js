c5b439d731540784ff01762541006c29a0bd48f9
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-21 18:48:36 +0300

    Import and Render Navbar Component to the DOM
     - import Navbar to App
    
     - render Navbar to the DOM via App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
