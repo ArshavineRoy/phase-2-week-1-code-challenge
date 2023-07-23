a213a41a6766f5ca8ff83cddf91fc239015e31ed
ArshavineRoy <arshavinewaema@gmail.com>
2023-07-23 03:50:04 +0300

    feat: real-time search for transactions
    - enable user to search for transactions and render results on the DOM
    in real-time as they type
    
    - Body and Search components updated to achieve this
    
    - Body
        - handleSearch function to handle the search input from the Search component
    
        - filteredData function to filter the data based on the search term
    
        - pass 'handleSearch' function as 'onSearch' prop to Search
    
        - pass 'filteredData' as 'data' prop to Transactions
    
    - SEARCH
        - receive onSearch prop function
    
        - Initialize the searchTerm state variable using the useState hook
    
        - create handleChange function that's called while a user is typing into the search bar
    
        - get the current value of the search input
    
        - update the searchTerm state with the current value
    
        - call onSearch prop with the current search term as an argument
    
        - handleChange function returned onChange for real-time search
    
        - bind search input value to the searchTerm state

--- src/Body.js
+++ src/Body.js
@@ -4,21 +4,27 @@ import Transactions from "./Transactions";
 import Form from "./Form";
 
 function Body() {
+  // State variables to manage API data and search term
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  // useEffect hook to fetch data when the component mounts (when page loads)
   useEffect(() => {
     fetchData();
   }, []);
 
+  // Function to fetch data from the server
   const fetchData = () => {
     fetch("http://localhost:3000/transactions")
       .then((response) => {
         if (!response.ok) {
+          // handle errors on fetch
           throw new Error("Error fetching data.");
         }
         return response.json();
       })
       .then((data) => {
+        // Set the fetched data in the state variable 'data'
         setData(data);
       })
       .catch((error) => {
@@ -26,10 +32,26 @@ function Body() {
       });
   };
 
+  // Function to handle the search input from the Search component
+  const handleSearch = (term) => {
+    // Set the search term in the state variable 'searchTerm'
+    setSearchTerm(term);
+  };
+
+  // Filter the data based on the search term
+  const filteredData = data.filter((transaction) =>
+    transaction.description?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <Search />
-      <Transactions data={data} />
+      {/* Render the Search component and pass 'handleSearch' function as 'onSearch' prop */}
+      <Search onSearch={handleSearch} />
+
+      {/* Render the Transactions component and pass 'filteredData' as 'data' prop */}
+      <Transactions data={filteredData} />
+
+      {/* Render the Form component */}
       <Form />
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+// The Search component receives onSearch prop function
+
+function Search({ onSearch }) {
+  // Initialize the searchTerm state variable using the useState hook
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // handleChange function called while a user is typing into the search bar
+  const handleChange = (e) => {
+    // Get the current value of the search input
+    const term = e.target.value;
+
+    // Update the searchTerm state with the current value
+    setSearchTerm(term);
+
+    /* Call onSearch prop with the current search term as an arg, passig it 
+    back to Body component for filtering */
+
+    onSearch(term);
+    onSearch(term);
+  };
+
   return (
     <div className="container-fluid">
-      <form className="d-flex search-bar" role="search">
+      <form className="d-flex search-bar">
         <input
           className="form-control me-2"
           type="search"
-          placeholder="Search Transactions"
+          placeholder="Search transactions by description "
           aria-label="Search"
+          value={searchTerm} // The search input value is bound to the searchTerm state
+          onChange={handleChange} // handleChange function used for real-time search
         />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
       </form>
     </div>
   );
